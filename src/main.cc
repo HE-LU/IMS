2b984de96b1e1c1cc053067b11205e9a131b000b
Lukáš Hermann <tuxilero@gmail.com>
2014-11-16 14:09:35 +0100

    update

--- src/main.cc
+++ src/main.cc
@@ -39,20 +39,18 @@
 // #define GENERATE_CAR_NIGHT		3.125	// 22:00 -  6:00	// TODO
 // #define GENERATE_ACCIDENT		40000	// TODO
 
-#define GENERATE_CAR_DAY		1.1		//  6:00 - 18:00	// TODO
-#define GENERATE_CAR_EVENING	1.2		// 18:00 - 22:00	// TODO
-#define GENERATE_CAR_NIGHT		3 		// 22:00 -  6:00	// TODO
-#define GENERATE_ACCIDENT		40000	// TODO
+#define GENERATE_CAR_DAY		0.67		//  6:00 - 18:00	// TODO
+#define GENERATE_CAR_EVENING	0.98		// 18:00 - 22:00	// TODO
+#define GENERATE_CAR_NIGHT		3.125 		// 22:00 -  6:00	// TODO
+#define GENERATE_ACCIDENT		60000
 
 #define PRAHA_BRNO				1
 #define BRNO_PRAHA				2
 
-const int EXITS_PRAHA_BRNO[] =   {2, 6, 8, 10, 15, 21, 29, 34, 41, 49, 56, 66, 75, 90, 104, 112/*2x*/, 119, 134, 141, 146, 153, 162, 168, 178, 182, 190, 194/*2x*/, 196/*2x*/, 201,202};
-const int EXITS_BRNO_PRAHA[] =   {0, 2, 7/*2x*/, 9/*2x*/, 13, 21, 25, 35, 41, 50, 57, 62, 69, 84, 91, 99, 113, 122, 137, 147, 154, 162, 169, 174, 182, 188, 191, 195, 197, 201/*2x*/, 202};
-const int ENTRIES_PRAHA_BRNO[] = {1, 2, 6, 8, 12, 15, 21, 29, 34, 41, 49, 56, 66, 81, 90, 104, 112, 119, 134, 141, 146, 153, 162, 168, 178, 182, 190, 194/*2x*/, 196/*2x*/, 201, 203};
-const int ENTRIES_BRNO_PRAHA[] = {2, 7/*2x*/, 9/*2x*/, 13, 21, 25, 35, 41, 50, 57, 62, 69, 84, 99, 113, 128, 137, 147, 154, 162, 169, 174, 182, 188, 193, 195, 197, 201};
-// const int EXITS_BRNO_PRAHA[] =   {1, 2/*2x*/, 6, 8, 12, 15, 21, 29, 34, 41, 49, 56, 66, 81, 90, 104, 112, 119, 134, 141, 146, 153, 162, 168, 178, 182, 190, 194/*2x*/, 196/*2x*/, 201, 203};
-// const int ENTRIES_BRNO_PRAHA[] = {2, 6, 8, 10, 15, 21, 29, 34, 41, 49, 56, 66, 75, 90, 104, 119, 134, 141, 146, 153, 162, 168, 178, 182, 190, 194/*2x*/, 196/*2x*/, 201};
+const int EXITS_PRAHA_BRNO[]	= {2, 6, 8, 10, 15, 21, 29, 34, 41, 49, 56, 66, 75, 90, 104, 112/*2x*/, 119, 134, 141, 146, 153, 162, 168, 178, 182, 190, 194/*2x*/, 196/*2x*/, 201, 202};
+const int EXITS_BRNO_PRAHA[]	= {0, 2, 7/*2x*/, 9/*2x*/, 13, 21, 25, 35, 41, 50, 57, 62, 69, 84, 91, 99, 113, 122, 137, 147, 154, 162, 169, 174, 182, 188, 191, 195, 197, 201/*2x*/, 202};
+const int ENTRIES_PRAHA_BRNO[]	= {1, 2, 6, 8, 12, 15, 21, 29, 34, 41, 49, 56, 66, 81, 90, 104, 112, 119, 134, 141, 146, 153, 162, 168, 178, 182, 190, 194/*2x*/, 196/*2x*/, 201, 203};
+const int ENTRIES_BRNO_PRAHA[]	= {2, 7/*2x*/, 9/*2x*/, 13, 21, 25, 35, 41, 50, 57, 62, 69, 84, 99, 113, 128, 137, 147, 154, 162, 169, 174, 182, 188, 193, 195, 197, 201};
 // ====== Definitions ======
 // =========================
 
@@ -71,7 +69,9 @@ const int ENTRIES_BRNO_PRAHA[] = {2, 7/*2x*/, 9/*2x*/, 13, 21, 25, 35, 41, 50, 5
 #define isEntryPrahaBrno(km) 	(std::find(std::begin(ENTRIES_PRAHA_BRNO), std::end(ENTRIES_PRAHA_BRNO), km) != std::end(ENTRIES_PRAHA_BRNO))
 #define isEntryBrnoPraha(km) 	(std::find(std::begin(ENTRIES_BRNO_PRAHA), std::end(ENTRIES_BRNO_PRAHA), km) != std::end(ENTRIES_BRNO_PRAHA))
 
-#define isUnderContruction(km) ((km > 0 && km < 1) || (km > 0 && km < 1)) ? true : false // TODO
+// #define isUnderContruction(km) ((km > 0 && km < 1) || (km > 0 && km < 1)) ? true : false // TODO
+#define isUnderContructionPrahaBrno(i) ((i == 38) || (i >= 40  && i <= 50) || (i >= 65 && i <= 77))
+#define isUnderContructionBrnoPraha(i) ((i >= 127 && i <= 138 ) || (i >= 153  && i <= 163))
 
 #define najezdPrahaBrno(x) \
 	((x < 0.0544595447339664) ?   0 : ((x < 0.108919089467933) ?   1 : ((x < 0.157776307081403) ?   2 : ((x < 0.200003078201715) ?   6 : ((x < 0.242229849322026) ?   8 : \
@@ -82,14 +82,6 @@ const int ENTRIES_BRNO_PRAHA[] = {2, 7/*2x*/, 9/*2x*/, 13, 21, 25, 35, 41, 50, 5
 	((x < 0.757877272097641)  ? 178 : ((x < 0.786016960891447) ? 182 : ((x < 0.822286796053745) ? 190 : ((x < 0.902339741123236) ? 194 : ((x < 0.959471164945439) ? 196 : \
 	((x < 0.97973558247272)   ? 201 : 203)))))))))))))))))))))))))))))))
 
-#define sjezdPrahaBrno(x) \
-	((isExitPrahaBrno(x)==0) ? 0   : ((x==2)   ? 0.0570318181583739 : ((x==6)   ? 0.051164877787119  : ((x==8)   ? 0.0442212571499306 : ((x==10)  ? 0.0442212571499306 : \
-	((x==15) ? 0.0463997617119603  : ((x==21)  ? 0.0411150023467761 : ((x==29)  ? 0.0265018077912968 : ((x==34)  ? 0.026063399060258  : ((x==41)  ? 0.0252929601873295 : \
-	((x==49)  ? 0.0238894075292831 : ((x==56)  ? 0.0236585982267656 : ((x==66)  ? 0.0229120139465554 : ((x==75)  ? 0.0218817534286142 : ((x==90)  ? 0.0220281045785345 : \
-	((x==104) ? 0.0232550043067211 : ((x==112) ? 0.0459052624520974 : ((x==119) ? 0.0229519865073266 : ((x==134) ? 0.022631561302435  : ((x==141) ? 0.0230164583795382 : \
-	((x==146) ? 0.0228230427629034 : ((x==153) ? 0.0238468560936235 : ((x==162) ? 0.0227953198578524 : ((x==168) ? 0.0245689410623933 : ((x==178) ? 0.0254483373993594 : \
-	((x==182) ? 0.0259699148455512 : ((x==190) ? 0.0294688033504743 : ((x==194) ? 0.0759659175894741  : ((x==196) ? 0.0838340648741767 : ((x==201) ? 0.0299149487061785 : 1))))))))))))))))))))))))))))))
-
 #define najezdBrnoPraha(x) \
 	((x < 0.022242614514056) ?   0 : ((x < 0.053596889439691) ?   2 : ((x < 0.141464541964048) ?   7 : ((x < 0.221085479592016) ?   9 : ((x < 0.251972143348501) ?  13 : \
 	((x < 0.279191573268318) ?  21 : ((x < 0.305864330592988) ?  25 : ((x < 0.331615380353033) ?  35 : ((x < 0.355507472994103) ?  41 : ((x < 0.380501695428358) ?  50 : \
@@ -97,6 +89,14 @@ const int ENTRIES_BRNO_PRAHA[] = {2, 7/*2x*/, 9/*2x*/, 13, 21, 25, 35, 41, 50, 5
 	((x < 0.523785325929039) ? 113 : ((x < 0.546719896855247) ? 128 : ((x < 0.570734298198075) ? 137 : ((x < 0.595531204936677) ? 147 : ((x < 0.620570026124058) ? 154 : \
 	((x < 0.647079930425945) ? 162 : ((x < 0.674397342454905) ? 169 : ((x < 0.702174256788809) ? 174 : ((x < 0.745267464128412) ? 182 : ((x < 0.79389970145865)  ? 188 : \
 	((x < 0.840248617776522) ? 193 : ((x < 0.886597534094395) ? 195 : ((x < 0.940224156036171) ? 197 : 201))))))))))))))))))))))))))))
+
+#define sjezdPrahaBrno(x) \
+	((isExitPrahaBrno(x)==0) ? 0   : ((x==2)   ? 0.0570318181583739 : ((x==6)   ? 0.051164877787119  : ((x==8)   ? 0.0442212571499306 : ((x==10)  ? 0.0442212571499306 : \
+	((x==15) ? 0.0463997617119603  : ((x==21)  ? 0.0411150023467761 : ((x==29)  ? 0.0265018077912968 : ((x==34)  ? 0.026063399060258  : ((x==41)  ? 0.0252929601873295 : \
+	((x==49)  ? 0.0238894075292831 : ((x==56)  ? 0.0236585982267656 : ((x==66)  ? 0.0229120139465554 : ((x==75)  ? 0.0218817534286142 : ((x==90)  ? 0.0220281045785345 : \
+	((x==104) ? 0.0232550043067211 : ((x==112) ? 0.0459052624520974 : ((x==119) ? 0.0229519865073266 : ((x==134) ? 0.022631561302435  : ((x==141) ? 0.0230164583795382 : \
+	((x==146) ? 0.0228230427629034 : ((x==153) ? 0.0238468560936235 : ((x==162) ? 0.0227953198578524 : ((x==168) ? 0.0245689410623933 : ((x==178) ? 0.0254483373993594 : \
+	((x==182) ? 0.0259699148455512 : ((x==190) ? 0.0294688033504743 : ((x==194) ? 0.0759659175894741  : ((x==196) ? 0.0838340648741767 : ((x==201) ? 0.0299149487061785 : 1))))))))))))))))))))))))))))))
 
 #define sjezdBrnoPraha(x) \
 	((isExitPrahaBrno(x)==0) ? 0   : ((x==0)   ? 0.0192204382699036 : ((x==2)   ? 0.0192204382699036 : ((x==7)   ? 0.0541881356011379  : ((x==9)   ? 0.0759287893155945 : \
@@ -106,13 +106,13 @@ const int ENTRIES_BRNO_PRAHA[] = {2, 7/*2x*/, 9/*2x*/, 13, 21, 25, 35, 41, 50, 5
 	((x==147) ? 0.0207514866701858 : ((x==154) ? 0.02142767051745   : ((x==162) ? 0.0216367152647044 : ((x==169) ? 0.0229079175406061 : ((x==174) ? 0.0236057065712457 : \
 	((x==182) ? 0.0240027748062541 : ((x==188) ? 0.037237993357282  : ((x==191) ? 0.0420244173607575 : ((x==195) ? 0.0400513385870911 : ((x==198) ? 0.0463401987209731 : \
 	((x==201) ? 0.0926803974419463 : ((x==202) ? 0.0516539059835847 : ((x==203) ? 0.0516539059835847 : 1)))))))))))))))))))))))))))))))))
-
 // ======== Macros =========
 // =========================
 
 
 // =========================
 // ======== Global =========
+int gDirection = PRAHA_BRNO;
 unsigned long gCounterCar = 0;
 unsigned long gCounterAccident = 0;
 
@@ -134,10 +134,8 @@ unsigned long gCounterKm194to196 = 0;
 
 // =========================
 // ======= Histogram =======
-Histogram		hZdrzeni("Doba cakania v kolone v sec", 0, 600, 20); 
-Histogram		hKilometry("kilometre s kolonou",0,1,HIGHWAY_LENGTH);
-
-// Histogram		hKm1to2("KM 1 - 2",0,1,1000000);
+Histogram					hZdrzeni("Doba cakania v kolone v sec", 0, 600, 20);
+Histogram					hKilometry("kilometre s kolonou", 0, 1, HIGHWAY_LENGTH);
 // ======= Histogram =======
 // =========================
 
@@ -166,34 +164,66 @@ class Car : public Process
 	void Behavior()
 	{
 		gCounterCar++;
-		mCurrentPosition = najezdPrahaBrno(Random());
+		if (gDirection == PRAHA_BRNO)
+			mCurrentPosition = najezdPrahaBrno(Random());
+		else
+			mCurrentPosition = najezdBrnoPraha(Random());
+
 		mEntryTime = Time;
 		gHighway[mCurrentPosition]->Enter(this, 1);
-		do
-		{
-			debugKm1to2(mCurrentPosition);
-			debugKm2to6(mCurrentPosition);
-			debugKm12to15(mCurrentPosition);
-			debugKm41to49(mCurrentPosition);
-			debugKm153to162(mCurrentPosition);
-			debugKm194to196(mCurrentPosition);
 
-			if ((Time - mEntryTime) != 0)
+		if (gDirection == PRAHA_BRNO)
+			do
 			{
-				hZdrzeni(Time - mEntryTime);
-				hKilometry(mCurrentPosition);
+				debugKm1to2(mCurrentPosition);
+				debugKm2to6(mCurrentPosition);
+				debugKm12to15(mCurrentPosition);
+				debugKm41to49(mCurrentPosition);
+				debugKm153to162(mCurrentPosition);
+				debugKm194to196(mCurrentPosition);
+
+				if ((Time - mEntryTime) != 0)
+				{
+					hZdrzeni(Time - mEntryTime);
+					hKilometry(mCurrentPosition);
+				}
+
+				mCurrentSpeed = Normal(1.0, 0);
+				mCurrentSpeed = 80 + mCurrentSpeed * 50;
+				Wait(1000 / mCurrentSpeed);
+
+				mCurrentPosition++;
+				mEntryTime = Time;
+				gHighway[mCurrentPosition]->Enter(this, 1);
+				gHighway[mCurrentPosition - 1]->Leave(1);
 			}
+			while (Random() > sjezdPrahaBrno(mCurrentPosition));
+		else
+			do
+			{
+				debugKm1to2(mCurrentPosition);
+				debugKm2to6(mCurrentPosition);
+				debugKm12to15(mCurrentPosition);
+				debugKm41to49(mCurrentPosition);
+				debugKm153to162(mCurrentPosition);
+				debugKm194to196(mCurrentPosition);
 
-			mCurrentSpeed = Normal(1.0, 0);
-			mCurrentSpeed = 80 + mCurrentSpeed * 50;
-			Wait(1000 / mCurrentSpeed);
+				if ((Time - mEntryTime) != 0)
+				{
+					hZdrzeni(Time - mEntryTime);
+					hKilometry(mCurrentPosition);
+				}
 
-			mCurrentPosition++;
-			mEntryTime = Time;
-			gHighway[mCurrentPosition]->Enter(this, 1);
-			gHighway[mCurrentPosition - 1]->Leave(1);
-		}
-		while (Random() > sjezdPrahaBrno(mCurrentPosition));
+				mCurrentSpeed = Normal(1.0, 0);
+				mCurrentSpeed = 80 + mCurrentSpeed * 50;
+				Wait(1000 / mCurrentSpeed);
+
+				mCurrentPosition++;
+				mEntryTime = Time;
+				gHighway[mCurrentPosition]->Enter(this, 1);
+				gHighway[mCurrentPosition - 1]->Leave(1);
+			}
+			while (Random() > sjezdPrahaBrno(mCurrentPosition));
 
 		gHighway[mCurrentPosition]->Leave(1);
 		Terminate();
@@ -240,6 +270,16 @@ class GeneratorCar : public Event
 			Activate(Time + Exponential(GENERATE_CAR_NIGHT));
 	}
 };
+
+class GeneratorProgress : public Event
+{
+	void Behavior()
+	{
+		std::cout << "Progress: " << Time / 3600 << " / 24" << std::endl;
+
+		Activate(Time + 3600);
+	}
+};
 // ======== Classes ========
 // =========================
 
@@ -253,16 +293,30 @@ void initHighway()
 	for (int i = 0; i < HIGHWAY_LENGTH; i++)
 	{
 		bool exit = false;
-		int maxSpeed  = 130;
-		int capacity  = CAPACITY;
+		int maxSpeed = 130;
+		int capacity = CAPACITY;
 
-		if (isExitPrahaBrno(i))
-			exit = true;
-
-		if (isUnderContruction(i))
+		if (gDirection == PRAHA_BRNO)
 		{
-			maxSpeed = 90;
-			capacity = CAPACITY_LIMITED;
+			if (isExitPrahaBrno(i))
+				exit = true;
+
+			if (isUnderContructionPrahaBrno(i))
+			{
+				maxSpeed = 90;
+				capacity = CAPACITY_LIMITED;
+			}
+		}
+		else
+		{
+			if (isExitBrnoPraha(i))
+				exit = true;
+
+			if (isUnderContructionBrnoPraha(i))
+			{
+				maxSpeed = 90;
+				capacity = CAPACITY_LIMITED;
+			}
 		}
 
 		tmp = new HighwayPart(capacity, maxSpeed, exit);
@@ -286,8 +340,10 @@ int main()
 	SetOutput(OUTPUT_FILE);
 	RandomSeed ( time(NULL) );
 
+	gDirection = PRAHA_BRNO;
 	initHighway();
 	Init(0, SIMULATION_DURATION * DAY_IN_SECONDS);
+	(new GeneratorProgress)->Activate();
 	(new GeneratorCar)->Activate();
 	(new GeneratorAccident)->Activate(Time + Exponential(GENERATE_ACCIDENT));
 
@@ -295,16 +351,23 @@ int main()
 
 	destroyHighway();
 
-	_Print("Dialnicou preslo %d osobnych aut\n",gCounterCar);
-  	_Print("Na dialnici sa stalo %d nehod\n\n",gCounterAccident);
+	_Print("\nDalnici projelo %d aut\n", gCounterCar);
+	_Print("Na dialnici sa stalo %d nehod\n\n", gCounterAccident);
 
 
-  	_Print("Na km   1 -   2 projelo %d aut. Mělo by projet 88 460\n",gCounterKm1to2);
-  	_Print("Na km   2 -   6 projelo %d aut. Mělo by projet 79 360\n",gCounterKm2to6);
-  	_Print("Na km  12 -  15 projelo %d aut. Mělo by projet 73 397\n",gCounterKm12to15);
-  	_Print("Na km  41 -  49 projelo %d aut. Mělo by projet 37 054\n",gCounterKm41to49);
-  	_Print("Na km 153 - 162 projelo %d aut. Mělo by projet 35 357\n",gCounterKm153to162);
-  	_Print("Na km 194 - 196 projelo %d aut. Mělo by projet 65 016\n",gCounterKm194to196); 	
+	// _Print("Na km   1 -   2 projelo %d aut. Mělo by projet 88 460\n",gCounterKm1to2);
+	// _Print("Na km   2 -   6 projelo %d aut. Mělo by projet 79 360\n",gCounterKm2to6);
+	// _Print("Na km  12 -  15 projelo %d aut. Mělo by projet 73 397\n",gCounterKm12to15);
+	// _Print("Na km  41 -  49 projelo %d aut. Mělo by projet 37 054\n",gCounterKm41to49);
+	// _Print("Na km 153 - 162 projelo %d aut. Mělo by projet 35 357\n",gCounterKm153to162);
+	// _Print("Na km 194 - 196 projelo %d aut. Mělo by projet 65 016\n",gCounterKm194to196);
+
+	_Print("Na km   1 -   2 projelo %d aut. Mělo by projet 44 230\n", gCounterKm1to2);
+	_Print("Na km   2 -   6 projelo %d aut. Mělo by projet 37 180\n", gCounterKm2to6);
+	_Print("Na km  12 -  15 projelo %d aut. Mělo by projet 36 698\n", gCounterKm12to15);
+	_Print("Na km  41 -  49 projelo %d aut. Mělo by projet 18 527\n", gCounterKm41to49);
+	_Print("Na km 153 - 162 projelo %d aut. Mělo by projet 17 678\n", gCounterKm153to162);
+	_Print("Na km 194 - 196 projelo %d aut. Mělo by projet 32 508\n\n", gCounterKm194to196);
 
 	// hZdrzeni.Output();
 	// hKilometry.Output();
